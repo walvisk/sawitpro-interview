7f1d653547d542467489f349d3a9b6f9dca2d908
walvisk <dedypuji@outlook.com>
2024-01-30 17:50:23 +0700

    init table users and user_logs

--- database.sql
+++ database.sql
@@ -7,11 +7,21 @@
   In this assignment we will use PostgreSQL as the database.
   */
 
-/** This is test table. Remove this table and replace with your own tables. */
-CREATE TABLE test (
-	id serial PRIMARY KEY,
-	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+CREATE TABLE users (
+    id serial primary key,
+    phone varchar(13) unique not null,
+    country_code varchar(3) not null default '+62',
+    password varchar(255) not null,
+    full_name varchar(64) not null,
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp,
+    deleted_at timestamptz default null
 );
 
-INSERT INTO test (name) VALUES ('test1');
-INSERT INTO test (name) VALUES ('test2');
+CREATE INDEX idx_phone ON users (phone);
+
+CREATE TABLE user_logs (
+    id serial primary key,
+    login_at timestamptz not null default current_timestamp,
+    user_id integer references users(id) on delete cascade
+);
